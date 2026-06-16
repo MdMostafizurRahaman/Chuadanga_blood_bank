dac8496bb12faf22188e8a7c44c2267927e845a8
Rakib <bsse1320@iit.du.ac.bd>
2026-06-16 13:00:46 +0600

    Move MONGO_URI to .env for security

--- backend/database.py
+++ backend/database.py
@@ -1,6 +1,10 @@
+import os
+from dotenv import load_dotenv
 from pymongo import MongoClient
 
-MONGO_URI = "mongodb+srv://bsse1320_db_user:ebzFDHCuPQeHvagY@cluster0.vgc9qm1.mongodb.net/"
+load_dotenv()
+
+MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017")
 client = MongoClient(MONGO_URI)
 db = client['blood_bank_db']
 
